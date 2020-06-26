0d746c9f183dfc837c61e1c409312df7d6586085
fawaz alomani <arabzoone@fawazs-MacBook-Air.local>
2020-06-26 21:55:18 +0300

    coderoad

---
+++ fawazfunctions.js
@@ -0,0 +1,39 @@
+function mycolor(){
+
+    let x = Math.floor(Math.random()*256);
+    let y = Math.floor(Math.random()*256);
+    let z = Math.floor(Math.random()*256);
+    let bg = "rgb(" + x + " , " + y + " , " + z +  ")";
+    
+    console.log(bg);
+    document.body.style.background=bg;
+    
+    }
+    
+    mycolor();
+    
+    
+    
+    
+    
+    
+    
+    function red(){
+    
+        let y = ["`3B5AFAF"];
+    
+        console.log(y);
+        document.body.style.background=y;
+    }
+    
+    blo();
+    
+    function black(){
+    
+        let x = ["#060606"];
+    
+        console.log(x);
+        document.body.style.background=x;
+    }
+    
+    black();
